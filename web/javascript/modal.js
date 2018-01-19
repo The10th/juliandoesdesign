1f0a8ba6c71380612890db3311c4fe7b97711ec1
Julian <jhvndev@gmail.com>
2018-01-19 01:18:08 -0800

    funsies

--- web/javascript/modal.js
+++ web/javascript/modal.js
@@ -1,10 +1,14 @@
+$(document).ready(function(){
+  console.info('Oh, why hello there! Thanks for taking a gander at the code in the inspector. Please enjoy the class names, fun declarations, and image sizes. I am not one to leave anyone in boredom, whilst inspecting in the...inspector...so here is a random factoid: If one were to bang their head against the wall for an hour, one could potentially burn 150 calories...');
+});
+
 $(document).delegate('[data-action=show-modal]', 'click', function(event){
+
+  event.stopPropagation();
+
   var $path = $(this).data('path');
 
-  $('[data-modal='+ $path +']').addClass('is-active').promise().done(function(){
-
-    $('body').addClass('u-noverFlow');
-  });
+  showGodTier($path);
 });
 
 $(document).delegate('[data-action=exit-modal]', 'click', function(event){
@@ -17,3 +21,12 @@ $(document).delegate('[data-action=exit-modal]', 'click', function(event){
     $modal.removeClass('is-active').removeAttr('style');
   });
 });
+
+
+function showGodTier(path) {
+
+  $('[data-modal='+ path +']').addClass('is-active').promise().done(function(){
+
+    $('body').addClass('u-noverFlow');
+  });
+}
