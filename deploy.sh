12ce4c478bd1ce14db23ea9ad6ff8f22bdae2388
Julian <jhvndev@gmail.com>
2018-01-19 19:23:41 -0800

    MOAR MOVINGG

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 s3cmd -P put *.html s3://www.juliandoesui.com/
+s3cmd -P put cmb.html s3://www.juliandoesui.com/cmb/index.html
+s3cmd -P put heap.html s3://www.juliandoesui.com/heap/index.html
 s3cmd -P --recursive put web s3://www.juliandoesui.com/
