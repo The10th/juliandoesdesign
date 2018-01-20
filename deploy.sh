412992b57fe1d6319e3855ddb8fa6493577d729b
Julian <jhvndev@gmail.com>
2018-01-20 13:16:53 -0800

    favis

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 s3cmd -P put *.html s3://www.juliandoesui.com/
+s3cmd -P put *.png s3://www.juliandoesui.com/
+s3cmd -P put *.ico s3://www.juliandoesui.com/
 s3cmd -P put cmb.html s3://www.juliandoesui.com/cmb/index.html
 s3cmd -P put heap.html s3://www.juliandoesui.com/heap/index.html
 s3cmd -P --recursive put web s3://www.juliandoesui.com/
