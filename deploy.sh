d87da7844e9d9db2f11e3bcf792752d2cbb3c8f1
Julian <jhvndev@gmail.com>
2018-01-21 21:37:19 -0800

    vendor and cleanup

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ s3cmd -P put *.png s3://www.juliandoesui.com/
 s3cmd -P put *.ico s3://www.juliandoesui.com/
 s3cmd -P put cmb.html s3://www.juliandoesui.com/cmb/index.html
 s3cmd -P put cleary.html s3://www.juliandoesui.com/cleary/index.html
+s3cmd -P put better.html s3://www.juliandoesui.com/better/index.html
 s3cmd -P put remind.html s3://www.juliandoesui.com/remind/index.html
 s3cmd -P put bluebottlecoffee.html s3://www.juliandoesui.com/bluebottle/index.html
 s3cmd -P put heap.html s3://www.juliandoesui.com/heap/index.html
+s3cmd -P put wonder.html s3://www.juliandoesui.com/wonder/index.html
 s3cmd -P --recursive put web s3://www.juliandoesui.com/
