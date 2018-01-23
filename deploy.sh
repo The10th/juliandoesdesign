6c25e62054aee38298b37ac0272330cb9bec566f
Julian <jhvndev@gmail.com>
2018-01-22 17:13:01 -0800

    vendor add

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ s3cmd -P put *.ico s3://www.juliandoesui.com/
 s3cmd -P put apptimize.html s3://www.juliandoesui.com/apptimize/index.html
 s3cmd -P put cmb.html s3://www.juliandoesui.com/cmb/index.html
 s3cmd -P put cleary.html s3://www.juliandoesui.com/cleary/index.html
+s3cmd -P put civicowl.html s3://www.juliandoesui.com/civicowl/index.html
 s3cmd -P put better.html s3://www.juliandoesui.com/better/index.html
 s3cmd -P put remind.html s3://www.juliandoesui.com/remind/index.html
 s3cmd -P put bluebottlecoffee.html s3://www.juliandoesui.com/bluebottle/index.html
