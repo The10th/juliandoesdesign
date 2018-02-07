60eab8b29e1102254e359c9f7e5c0662113289e7
Julian <jhvndev@gmail.com>
2018-02-07 15:41:36 -0800

    deploy things

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,10 @@ s3cmd -P put preview/apptimize.html s3://www.juliandoesui.com/apptimize/index.ht
 s3cmd -P put preview/cmb.html s3://www.juliandoesui.com/cmb/index.html
 s3cmd -P put preview/civicowl.html s3://www.juliandoesui.com/civicowl/index.html
 s3cmd -P put preview/creativemarket.html s3://www.juliandoesui.com/creativemarket/index.html
-s3cmd -P put preview/ck.html s3://www.juliandoesui.com/ck/index.html
 s3cmd -P put preview/better.html s3://www.juliandoesui.com/better/index.html
 s3cmd -P put preview/hearsay.html s3://www.juliandoesui.com/hearsay/index.html
 s3cmd -P put preview/hinge.html s3://www.juliandoesui.com/hinge/index.html
 s3cmd -P put preview/flexport.html s3://www.juliandoesui.com/flexport/index.html
-s3cmd -P put preview/remind.html s3://www.juliandoesui.com/remind/index.html
-s3cmd -P put preview/bluebottlecoffee.html s3://www.juliandoesui.com/bluebottle/index.html
-s3cmd -P put preview/heap.html s3://www.juliandoesui.com/heap/index.html
+s3cmd -P put preview/taskrabbit.html s3://www.juliandoesui.com/taskrabbit/index.html
 s3cmd -P put preview/wonder.html s3://www.juliandoesui.com/wonder/index.html
 s3cmd -P --recursive put web s3://www.juliandoesui.com/
