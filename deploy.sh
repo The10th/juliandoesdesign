6e0fa821029cfaaf4ba537431cb5a571309f553d
Julian <jhvndev@gmail.com>
2018-02-16 03:53:16 -0800

    lots

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ s3cmd -P put preview/elliemae.html s3://www.juliandoesui.com/elliemae/index.html
 s3cmd -P put preview/hearsay.html s3://www.juliandoesui.com/hearsay/index.html
 s3cmd -P put preview/hinge.html s3://www.juliandoesui.com/hinge/index.html
 s3cmd -P put preview/flexport.html s3://www.juliandoesui.com/flexport/index.html
+s3cmd -P put preview/quip.html s3://www.juliandoesui.com/quip/index.html
 s3cmd -P put preview/taskrabbit.html s3://www.juliandoesui.com/taskrabbit/index.html
 s3cmd -P put preview/wonder.html s3://www.juliandoesui.com/wonder/index.html
 s3cmd -P --recursive put web s3://www.juliandoesui.com/
