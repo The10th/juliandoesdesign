6b69d68db9de5636fffbec5ebbdee5fc713a9fe0
Julian <jhvndev@gmail.com>
2018-02-16 05:42:54 -0800

    vendor removals, layout shifts, vendor add, responsiveness

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,8 @@ s3cmd -P put *.png s3://www.juliandoesui.com/
 s3cmd -P put *.ico s3://www.juliandoesui.com/
 s3cmd -P put preview/apptimize.html s3://www.juliandoesui.com/apptimize/index.html
 s3cmd -P put preview/autodesk.html s3://www.juliandoesui.com/autodesk/index.html
-s3cmd -P put preview/cmb.html s3://www.juliandoesui.com/cmb/index.html
-s3cmd -P put preview/civicowl.html s3://www.juliandoesui.com/civicowl/index.html
 s3cmd -P put preview/creativemarket.html s3://www.juliandoesui.com/creativemarket/index.html
-s3cmd -P put preview/better.html s3://www.juliandoesui.com/better/index.html
 s3cmd -P put preview/elliemae.html s3://www.juliandoesui.com/elliemae/index.html
-s3cmd -P put preview/hearsay.html s3://www.juliandoesui.com/hearsay/index.html
 s3cmd -P put preview/hinge.html s3://www.juliandoesui.com/hinge/index.html
 s3cmd -P put preview/flexport.html s3://www.juliandoesui.com/flexport/index.html
 s3cmd -P put preview/quip.html s3://www.juliandoesui.com/quip/index.html
