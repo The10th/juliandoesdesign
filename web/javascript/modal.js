2fac3ba600ceb4ea0ee87487474325501fbb2615
Julian <jhvndev@gmail.com>
2018-02-16 13:46:30 -0800

    facts

--- web/javascript/modal.js
+++ web/javascript/modal.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
-  console.info('Oh, why hello there! Thanks for taking a gander at the code in the inspector. Please enjoy the class names, fun declarations, and image sizes. I am not one to leave anyone in boredom, whilst inspecting in the...inspector...so here is a random factoid: If one were to bang their head against the wall for an hour, one could potentially burn 150 calories...');
-  console.log('%c WOOOO!!!!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+
+  var console_welcome = 'Oh, why hello there!\r\n\r\n';
+  var console_message = 'Thanks for taking a gander at my bit code in this here fine inspector. Please enjoy the class names, fun declarations, and image sizes. I am not one to leave anyone in boredom, whilst inspecting in the...inspector...so here is a random thing to read: (psst, refresh for more things):\r\n\r\n';
+  var welcome_style   = 'font-weight: 600; font-size: 24px; color: rgba(255,255,255,.4); text-shadow: 0px -1px 0 #9C27B0, -1px 2px 0 rgb(64, 235, 245), 1px 4px 0 rgb(255, 0, 253);';
+  var message_style   = 'color: #1d2647; font-size: 12px;';
+  var info_style      = 'color: #1d2647; font-weight: 600;';
+  var facts           = Array('The collective noun for a group of geese on the ground is a \'gaggle\', unfortunately a group of sheep is not known as a \'shaggle\'.',
+                              'While \'Internet Addiction Disorder\' is not listed in the DSM-5 as a disorder, Internet Gaming Addiction somehow is. ¯\_(ツ)_/¯',
+                              'Movie trailers were traditionally shown after the feature presentation...hence the word: trailer',
+                              'Han Shot First. Always did, always has...never not that.',
+                              'An individual is, apparently, incapable of snoring & dreaming simultaneously. Drooling can, however, still occur.',
+                              'On average, roughly 60% of the time, a CSS codebase will work everytime.',
+                              'The Twitter bird has a name, it\'s...it\'s Larry.',
+                              'There is a rather unfortunate phobia that exists: Panphobia. It is not, as the name would lead you to think, the fear of Peter Pan, but a literal fear of #allTheThings.',
+                              'Originally, the CSS em unit was derived from the width of the capital letter M, which is where its name came from...lol, ikr?',
+                              '#badA55 is a valid hexcode for a CSS color declaration. It\'s lime green - there, now you won\'t have to put your eyes through having to look at it.',
+                              'The first Carnival in Brazil dates back as far as 1723.',
+                              'The ancient and revered Japanese art of \'Karaoke\' means: An Empty Orchestra.',
+                              'Coke does NOT taste like Pepsi.',
+                              'Julian Lennon was forced to bid & purchase letters he wrote to his own father, John Lennon, at auction because Yoko Ono wouldn\'t give the letters to him. SUPER FUN FACT: I am named after Julian Lennon...I think...',
+                              'Haiku:\r\nWaving Jedi hand,\r\nyou want to reach out to me,\r\nlet\'s grab some coffee.');
+
+  var console_factoid = facts[Math.floor(Math.random()*facts.length)];
+
+  console.info('%c'+ console_welcome +'' + '%c'+ console_message +'' + '%c'+ console_factoid +'', welcome_style , message_style , info_style);
 });
 
 $(document).delegate('[data-action=show-modal]', 'click', function(event){
