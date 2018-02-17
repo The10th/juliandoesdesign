1ae86a74851960266321763aa2829713c7c25029
Julian <jhvndev@gmail.com>
2018-02-16 16:04:45 -0800

    deploy-y things

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ s3cmd -P put preview/hinge.html s3://www.juliandoesui.com/hinge/index.html
 s3cmd -P put preview/flexport.html s3://www.juliandoesui.com/flexport/index.html
 s3cmd -P put preview/fountain.html s3://www.juliandoesui.com/fountain/index.html
 s3cmd -P put preview/quip.html s3://www.juliandoesui.com/quip/index.html
+s3cmd -P put preview/stackshare.html s3://www.juliandoesui.com/stackshare/index.html
 s3cmd -P put preview/taskrabbit.html s3://www.juliandoesui.com/taskrabbit/index.html
 s3cmd -P put preview/trim.html s3://www.juliandoesui.com/trim/index.html
 s3cmd -P put preview/wonder.html s3://www.juliandoesui.com/wonder/index.html
